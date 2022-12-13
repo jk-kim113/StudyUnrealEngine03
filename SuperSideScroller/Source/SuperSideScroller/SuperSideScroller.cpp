6c441de530485933525ca73f74fffe5d97e2327d
jkkim113 <rlawhdrud113@gmail.com>
2022-12-13 16:56:47 +0900

    chapter10

---
+++ SuperSideScroller/Source/SuperSideScroller/SuperSideScroller.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "SuperSideScroller.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SuperSideScroller, "SuperSideScroller" );
+ 
